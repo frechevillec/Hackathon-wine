1b58ff265f8a512a61ce9a253a6902f0e7df1adc
Jerome Raffin <raffin.jerome@gmail.com>
2017-02-17 13:00:29 +0100

    marker sur la map

--- app/components/map/mapController.js
+++ app/components/map/mapController.js
@@ -5,23 +5,42 @@
 // On crée notre component marketList et on lui associe son template et son controller
 app.component("map", {
    	templateUrl: "/app/components/map/mapTemplate.html",
-    controller: function () {
-      let map = L.map('map', {
-        center: [44.83, -0.57],
-        zoom: 14,
-        zoomControl: false,
-        autoDiscover: true
-    });
+    controller: ['$http',
+    function ($http) {
 
-    L.control.zoom({
-            position: 'bottomright'
-        }).addTo(map);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: '',
-    id: 'mapbox.streets'
-    }).addTo(map);	
-}})
+        $http.get('http://wine.wildcodeschool.fr/api/v1/markets/').then((response) => {
+            this.markets = response.data;
 
+            let map = L.map('map', {
+                center: [44.83, -0.57],
+                zoom: 13,
+                zoomControl: false,
+                autoDiscover: true
+            });
+
+            console.log(Number(this.markets[0].position.split(",")[0]));
+
+            for(let i = 0; i < this.markets.length; i++) {
+                let marker = new L.marker([Number(this.markets[i].position.split(",")[0]), Number(this.markets[i].position.split(",")[1])]).bindPopup(this.markets[i].name).addTo(map);
+            }
+
+            // this.markets.forEach((position) => {
+            //     return L.marker(position).addTo(map);
+            // });
+
+            L.control.zoom({
+                position: 'bottomright'
+            }).addTo(map);
+
+            L.marker([44.83, -0.57]).addTo(map);
+
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: '',
+                id: 'mapbox.streets'
+            }).addTo(map);
+        })
+    }]
+})
 
 })(angular.module("map"))
